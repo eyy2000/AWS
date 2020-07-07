4a540d3878818ec1856d4c7586d348e2177f1584
Eric Yu <yya146@sfu.ca>
2020-07-07 02:11:56 -0700

    somewhat working index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 window.onload = async() => {
 	let data = await promisOfData;
+	document.body.innerHTML = "visitor count: " + data.body.body;
 }
 
 const promisOfData = fetch('https://2l10lnusjb.execute-api.us-west-2.amazonaws.com/dev/visitorcount')
 .then(response =>{
-	document.getElementById('body').innerHTML = "visitor count: ";
-	console.log(response.json());
 	return response.json();
 })
