32407cd02e81bfa0cb4fceaec713efc136f068e5
Eric Yu <yya146@sfu.ca>
2020-07-07 02:02:35 -0700

    update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-fetch('https://2l10lnusjb.execute-api.us-west-2.amazonaws.com/dev/visitorcount')
+window.onload = async() => {
+	let data = await promisOfData;
+}
+
+const promisOfData = fetch('https://2l10lnusjb.execute-api.us-west-2.amazonaws.com/dev/visitorcount')
 .then(response =>{
 	document.getElementById('body').innerHTML = "visitor count: ";
 	console.log(response.json());
