3a2799c06fae4c0cb9ff9dad17ca52e000e2f1a2
Eric Yu <yya146@sfu.ca>
2020-07-07 13:34:32 -0700

    added lambda function

---
+++ lambda_function.py
@@ -0,0 +1,41 @@
+import json
+import boto3
+from decimal import Decimal
+
+client = boto3.resource('dynamodb')
+table = client.Table('Test')
+
+
+def lambda_handler(event, context):
+    print("lambda_handler_start")
+    visitor_num = 0
+    print("visitor_number")
+    incr = 1
+    response = table.update_item(
+        Key={
+            'k': 'visitors'
+        },
+        UpdateExpression="set v = v + :incr",
+        ExpressionAttributeValues={
+            ':incr': Decimal(incr)
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+    
+    response = table.get_item(
+    Key = {
+        'k': 'visitors'
+    }
+    )
+    
+    item = response['Item']
+    v = item['v']
+    
+    response = {
+        'body': {
+            'statusCode': 200,
+            'body': v
+        }
+    }
+    
+    return response
